e32d33a161891819211a4ca9b93674cda49cfee9
alxayeed <alxayeed@gmail.com>
2021-06-28 17:32:06 +0600

    getting paragraphs and runs

--- pyword-text/word_to_text.py
+++ pyword-text/word_to_text.py
@@ -1,4 +1,20 @@
 import docx
+import logging
+
+logger = logging.getLogger('py-word   ')
+logger.setLevel(level=logging.DEBUG)
+
 
 doc_file = docx.Document('file/demo.docx')
-print(doc_file.paragraphs[0].text)
+
+paragraphs = doc_file.paragraphs
+# get number of paragraph in the word file
+logging.info(f'Total paragraph : {len(paragraphs)}')
+
+# print all paragraphs text
+for p in paragraphs:
+    logger.info(p.text)
+
+# Runs means different format of text, eg- normal, bold, italic etc
+# print runs
+logger.info(paragraphs[2].runs[1].text)
